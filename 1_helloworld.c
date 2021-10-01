6275972170fbedffa66fc871f3e441473c658b6a
Ahmed Tariq <78534449+AhmedTariq01@users.noreply.github.com>
2021-10-01 16:15:35 +0500

    change

--- 1_helloworld.c
+++ 1_helloworld.c
@@ -1,9 +1,8 @@
 #include<stdio.h>   // include
 int main()          // main function 
 {
-    printf("Hello world !");
+    printf("Hello !");
 }
-
 /*
 compile code using:         gcc filename.c
 execute a executable using: ./a.exe (windows) OR ./a.out (linux)
