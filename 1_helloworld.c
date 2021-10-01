930ffc5332cad2d12954763cebccba775002a86f
LazyCoder65 <72249468+LazyCoder65@users.noreply.github.com>
2021-10-01 14:05:07 +0530

    Add files via upload

---
+++ 1_helloworld.c
@@ -0,0 +1,10 @@
+#include<stdio.h>   // include
+int main()          // main function 
+{
+    printf("Hello world !");
+}
+
+/*
+compile code using:         gcc filename.c
+execute a executable using: ./a.exe (windows) OR ./a.out (linux)
+*/
---
+++ 2_datatypes.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    printf("DATA TYPES IN C: \n\t1.BASIC\n\t eg: int,signed int, unsigned int, char, unsigned char, signed char, short, unsigned short, long, unsigned long\n\t2.FLOATING\n\t eg: float, double, long double\n\t3.VOID\n\t4.DERIVED\n\t eg: pointers, arrays, structures, union, functions etc");
+    int a=12;
+    printf("%d",a);
+    float b = 3.14;
+    printf("%f",b);
+    char x = 'n';
+    printf("%c",x);
+
+}
