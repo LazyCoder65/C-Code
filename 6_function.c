eff06c7c393d66aa5554359e981a961307779927
duddlala0 <91423400+duddlala0@users.noreply.github.com>
2021-10-02 10:38:01 +0530

    Create 6_function.c

---
+++ 6_function.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+//function created to print my name
+function dudd(){
+  printf("Hello guys ,my name is dudd");
+}
+
+//main function is called to compile and execute c program
+int main(){
+  clrscr();//function called to clear the entire screen
+  dudd(); // function called to print my name
+  getch();//function to hold the screen
+  return 0;
+}
